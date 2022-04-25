0ce78b08cf78baced30870cf638e6da710758131
Steigner <200543@seznam.cz>
2022-04-25 21:09:25 +0200

    fix

--- app/flask_server/app/graph/graph_app.py
+++ app/flask_server/app/graph/graph_app.py
@@ -14,7 +14,7 @@ class Show_PointCloud(object):
             points = np.asarray(pcd_o.points)
             colours = np.asarray(pcd_o.colors)
 
-            cut = np.where(points[:, 2] > 1)
+            cut = np.where((points[:, 2] > 1) | (np.isnan(points[:,1])))
 
             points = np.delete(points, cut, axis=0)
             colours = np.delete(colours, cut, axis=0)
@@ -22,19 +22,11 @@ class Show_PointCloud(object):
             s_point = [0.025268396, 0.085912548, 0.50927734]
 
         else:
-            # color_raw = o3d.io.read_image("face.jpg")
-            # depth_raw = o3d.io.read_image("face.png")
-
-            # realworld test data
-            # point = [170, 312]
-
             color_raw = o3d.io.read_image("app/graph/face.jpg")
             depth_raw = o3d.io.read_image("app/graph/face.png")
 
             imge = np.asarray(color_raw)
             imge[point[1], point[0]] = [255, 255, 255]
-
-            # print(point[1], point[0])
 
             color_raw = o3d.geometry.Image(imge)
 
@@ -59,10 +51,6 @@ class Show_PointCloud(object):
                 )
             )
 
-            # tup = [6038]
-            # print(tup)
-            # print(pcd.points[tup[0][0]])
-
             # one of the possibility is set angle of motion to nostril by estimated normals
             # pcd.estimate_normals(
             #     search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
@@ -71,11 +59,7 @@ class Show_PointCloud(object):
             colours = np.asarray(pcd.colors)
             normals = np.asarray(pcd.normals)
 
-            # print(normals[tup[0]][0], normals[tup[0]][1], normals[tup[0]][2])
-
             s_point = pcd.points[tup[0][0]]
-            # s_point = pcd.points[tup[0]]
-            # s_point = [0.025268396, 0.085912548, 0.50927734]
 
         # this is due to color in plotly graphs
         pp_colours = colours[:] * 255.0
@@ -87,8 +71,3 @@ class Show_PointCloud(object):
         ]
 
         return points[:, 0], points[:, 1], points[:, 2], col, s_point
-
-
-# TESTING PURPOSE
-if __name__ == "__main__":
-    Show_PointCloud().load_pc()
